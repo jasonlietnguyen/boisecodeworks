58e40ad7d8a6854b7fddb36a5a27d91c8752af77
Jason Nguyen <jason123nguyen@gmail.com>
2016-07-31 02:44:11 -0600

    test 1

---
+++ js/way-point.js
@@ -0,0 +1,231 @@
+(function(){
+
+new Waypoint({
+    element: document.getElementsByClassName("fade1"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade2"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade3"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade4"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade5"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade6"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade7"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInRight")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade8"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade9"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade10"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInLeft")
+    	$(this.element).removeClass('hidden')
+    }
+})
+
+new Waypoint({
+    element: document.getElementsByClassName("fade11"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInLeft")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade12"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInLeft")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade13"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade14"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInLeft")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade15"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInRight")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade16"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade17"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade18"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade19"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade20"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("zoomIn")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade21"),
+    offset: 700,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("zoomIn")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade22"),
+    offset: 550,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade23"),
+    offset: 550,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInUp")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade24"),
+    offset: 500,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("tada")
+    	$(this.element).removeClass('hidden')
+    }
+})
+new Waypoint({
+    element: document.getElementsByClassName("fade25"),
+    offset: 600,
+    handler: function(direction){
+    	console.log(this.element.id + ' triggers at ' + this.triggerPoint)
+        $(this.element).addClass("fadeInDown")
+    	$(this.element).removeClass('hidden')
+    }
+})
+
+
+}())
